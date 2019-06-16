715f0a0c74cf9106d14aee087b1a2ff197cc8333
Jee Vang, Ph.D <vangjee@gmail.com>
2019-06-16 19:04:34 -0400

    add app config demo

---
+++ app-configs/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { environment } from './../environments/environment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app-configs';
+  url: string;
+  constructor() {
+    this.url = environment.baseUrl;
+  }
+}
---
+++ app-configs/src/environments/environment.stage.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  baseUrl: 'http://staging:5000'
+};
