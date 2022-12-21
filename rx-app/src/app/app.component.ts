67c164c0788c2c43e0161e228afd1632e76e2c6e
Jee Vang, Ph.D <vangjee@gmail.com>
2022-12-21 12:56:08 -0500

    add

--- rx-app/src/app/app.component.ts
+++ rx-app/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, AfterViewInit } from '@angular/core';
-import { of, forkJoin, fromEvent, EMPTY, } from 'rxjs';
-import { map, tap, flatMap, mapTo, scan, filter, debounceTime, concatMap, expand, toArray} from 'rxjs/operators';
+import { of, forkJoin, fromEvent, EMPTY, from, interval, throwError, zip, } from 'rxjs';
+import { map, tap, flatMap, mapTo, scan, filter, debounceTime, concatMap, expand, toArray, catchError, mergeMap, switchMap, take, retry, share} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +11,92 @@ export class AppComponent implements AfterViewInit {
   title = 'rx-app';
 
   ngAfterViewInit(): void {
-    this.doExpandDemo()
+    this.doZip()
+  }
+
+  private doZip(): void {
+    const ages = of(25, 34, 33);
+    const names = of('john', 'jack', 'jake');
+
+    zip(ages, names)
+      .pipe(
+        map(([age, name]) => ({name, age}))
+      )
+      .subscribe(x => console.log(x));
+  }
+
+  /**
+   * Doesn't work according to latest docs. Pretty neat though.
+   */
+  private doShare(): void {
+    const source = interval(1000)
+      .pipe(
+        tap(n => console.log(n)),
+        take(5),
+        share()
+      );
+
+    source.subscribe(
+      n => `1: ${n}`,
+      e => console.error(e)
+    );
+
+    source.subscribe(
+      n => `2: ${n}`,
+      e => console.error(e)
+    );
+  }
+
+  private doRetry(): void {
+    interval(1000)
+      .pipe(
+        mergeMap(v => v > 5 ? throwError(() => 'bad') : of(v)),
+        retry(2)
+      )
+      .subscribe(
+        v => console.log(v),
+        e => console.error(`${e}`)
+      );
+  }
+
+  private doInterval(): void {
+    interval(1000)
+      .pipe(take(5))
+      .subscribe(r => console.log(r));
+  }
+
+  private doSwitchMap(): void {
+    of(1, 2, 3)
+      .pipe(
+        switchMap(x => of(x + 1, x + 2, x + 3))
+      )
+      .subscribe(r => console.log(r));
+  }
+
+  /**
+   * Convert a promise to an observable.
+   */
+  private doConvertPromise(): void {
+    const p = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('i am a promise');
+      }, 300);
+    });
+
+    from(p).subscribe(r => console.log(r));
+  }
+
+  private doCatch(): void {
+    of(1, 2, 3, 4, 5).pipe(
+      map(n => {
+        if (n === 4) {
+          throw `${n}`;
+        }
+        return n;
+      }),
+      catchError(e => of('?'))
+    )
+    .subscribe(r => console.log(r));
   }
 
   /**
@@ -21,7 +106,7 @@ export class AppComponent implements AfterViewInit {
    * - https://stackoverflow.com/questions/43430536/rxjs-expand-array-from-promise
    * - https://ncjamieson.com/understanding-expand/
    */
-  private doExpandDemo(): void {
+  private doExpand(): void {
     const stuff = of({item: 0})
     .pipe(
       expand((data) => data.item < 10 ? of({item: data.item + 1}) : EMPTY),
@@ -30,7 +115,7 @@ export class AppComponent implements AfterViewInit {
     stuff.subscribe((data) => console.log(data));
   }
 
-  private doScanDemo(): void {
+  private doScan(): void {
     fromEvent(document, 'click')
       .pipe(
         mapTo(1),
@@ -44,13 +129,13 @@ export class AppComponent implements AfterViewInit {
       );
   }
 
-  private doTapDemo(): void {
+  private doTap(): void {
     const a = of('a').pipe(tap(r => console.log(r)));
     const b = of('b').pipe(tap(r => console.log(r)));
     const c = of('c')
       .pipe(
         tap(r => console.log(r)),
-        flatMap(() => of('d'))
+        mergeMap(() => of('d'))
       );
     forkJoin([a, b, c])
       .subscribe(
@@ -59,12 +144,12 @@ export class AppComponent implements AfterViewInit {
       );
   }
 
-  private doForkJoinDemo(): void {
+  private doForkJoin(): void {
     const a = of('a');
     const b = of('b');
     const c = of('c')
       .pipe(
-        flatMap(() => of('d'))
+        mergeMap(() => of('d'))
       );
     forkJoin([a, b, c])
       .subscribe(
@@ -73,21 +158,38 @@ export class AppComponent implements AfterViewInit {
       );
   }
 
-  private doFlatMapDemo(): void {
+  /**
+   * flatMap is deprecated use mergeMap
+   */
+  private doFlatMap(): void {
     of('a')
-    .pipe(
-      flatMap(r => {
-        console.log(r);
-        return of('b');
-      }),
-      flatMap(r => {
-        console.log(r);
-        return of('c');
-      })
-    )
-    .subscribe(
-      r => console.log(r),
-      e => console.error(e)
-    );
+      .pipe(
+        flatMap(r => {
+          console.log(r);
+          return of('b');
+        }),
+        flatMap(r => {
+          console.log(r);
+          return of('c');
+        })
+      )
+      .subscribe(
+        r => console.log(r),
+        e => console.error(e)
+      );
+  }
+
+  private doMergeMap(): void {
+    of('a')
+      .pipe(
+        tap(r => console.log(r)),
+        mergeMap(r => of('b')),
+        tap(r => console.log(r)),
+        mergeMap(r => of('c'))
+      )
+      .subscribe(
+        r => console.log(r),
+        e => console.error(e)
+      );
   }
 }
