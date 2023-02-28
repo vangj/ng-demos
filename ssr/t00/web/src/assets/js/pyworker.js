5bb86a517c04e3dcf4b688e94795d63599165262
Jee Vang, Ph.D <vangjee@gmail.com>
2023-02-28 14:24:27 -0500

    add support files

---
+++ ssr/t00/web/src/assets/js/pyworker.js
@@ -0,0 +1,28 @@
+const pyodideWorker = new Worker('./js/webworker.js');
+
+const callbacks = {};
+
+pyodideWorker.onmessage = (event) => {
+  const { id, ...data } = event.data;
+  const onSuccess = callbacks[id];
+  delete callbacks[id];
+  onSuccess(data);
+};
+
+const asyncRun = (() => {
+  let id = 0; // identify a Promise
+  return (script, context) => {
+    // the id could be generated more carefully
+    id = (id + 1) % Number.MAX_SAFE_INTEGER;
+    return new Promise((onSuccess) => {
+      callbacks[id] = onSuccess;
+      pyodideWorker.postMessage({
+        ...context,
+        python: script,
+        id,
+      });
+    });
+  };
+})();
+
+export { asyncRun };
