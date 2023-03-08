9e8e8d525029a2dc22f87d84499fda351431cc1b
Jee Vang, Ph.D <vangjee@gmail.com>
2023-03-08 15:28:55 -0500

    add dummy web worker

--- pyodide/t00/src/app/app.component.ts
+++ pyodide/t00/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 't00';
+export class AppComponent implements OnInit {
+  worker?: Worker;
+
+  constructor(@Inject(PLATFORM_ID) private readonly platformId: any) {
+    if (isPlatformBrowser(platformId)) {
+      console.log('isPlatformBrowser');
+      this.worker = new Worker(new URL('./app.worker', import.meta.url));
+      this.worker.onmessage = ({ data }) => {
+        console.log(`page got message: ${data}`);
+      };
+    }
+  }
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      if (this.worker) {
+        this.worker.postMessage('hello');
+      }
+    }
+  }
 }
